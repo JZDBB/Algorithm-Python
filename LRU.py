1b5967754a57db567998ccac89392f0cdf9e46f8
XuYining <oxuyining@gmail.com>
2018-11-06 09:57:56 +0800

    LRU web

---
+++ LRU.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+from collections import OrderedDict
+
+
+class LRUCache(OrderedDict):
+    def __init__(self, size=5):
+        self.size = size,
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if self.cache.has_key(key):
+            val = self.cache.pop(key)
+            self.cache[key] = val
+        else:
+            val = None
+
+        return val
+
+    def set(self, key, val):
+        if self.cache.has_key(key):
+            val = self.cache.pop(key)
+            self.cache[key] = val
+        else:
+            if len(self.cache) == self.size:
+                self.cache.popitem(last=False)
+                self.cache[key] = val
+            else:
+                self.cache[key] = val
+
+
+if __name__ == '__main__':
+    """ test """
+    cache = LRUCache(6)
+
+    for i in range(10):
+        cache.set(i, i)
+
+    for i in range(10):
+        import random
+
+        i = random.randint(1, 20)
+        print('cache', cache.cache.keys())
+        if cache.get(i):
+            print('hit, %s\n' % i)
+        else:
+            print('not hit, %s\n' % i)
+
+
