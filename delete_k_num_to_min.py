d6f7066298c7aad046c034a956324e6200c9fdbb
XuYining <oxuyining@gmail.com>
2018-11-13 23:31:40 +0800

    removekdigit dubug

--- delete_k_num_to_min.py
+++ delete_k_num_to_min.py
@@ -7,27 +7,27 @@ def removeKdigits(num, k):
     list_num = list(num)
     list_result = []
     curr = list_num.pop(0)
+    list_result.append(curr)
+    count = 0
     if k == 0:
         return num
-    for i in range(k):
+    for i in range(len(list(num))):
         if (list_num):
             next = list_num.pop(0)
         else:
             next = '0'
-            list_result.append(next)
+
         if curr > next:
-            try:
-                list_result.pop()
-                list_result.append(next)
-            except:
-                list_result.append(next)
+            list_result.append(next)
+            count += 1
+        elif curr == next:
+            list_result.append(next)
         else:
-            try:
-                list_result.pop()
-                list_result.append(curr)
-            except:
-                list_result.append(curr)
-        curr = list_result[-1]
+            list_result.append(curr)
+            list_result.append(next)
+        if count >= k:
+            break
+        curr = list_result.pop()
     list_result.extend(list_num)
     str_result = "".join(a for a in list_result)
     a = int(str_result)
