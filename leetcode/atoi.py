32a2dc47ab2e3200f6a0974534e13ba3c556697c
XuYining <oxuyining@gmail.com>
2019-04-10 14:51:14 +0800

    atoi debug ok

--- leetcode/atoi.py
+++ leetcode/atoi.py
@@ -1,38 +1,38 @@
 
 def atoi(s):
     flag = True
-    s = s.replace(" ", '')
-    ls = list(s)
+    s = s.lstrip()
+    ls = s.split(" ")
     num = 0
-    if ls:
-        if ls[0] == "-":
+    ceil = list(ls[0])
+    if ceil:
+        if ceil[0] == "-":
             flag = False
-            del ls[0]
-        elif ls[0] == "+":
-            del ls[0]
-        for i in range(len(ls)):
+            del ceil[0]
+        elif ceil[0] == "+":
+            del ceil[0]
+        for i in ceil:
             try:
-                a = int(ls[i])
+                a = int(i)
+                num = num * 10 + a
             except:
                 if flag:
-                    if num > 2 ** 31 - 1:
-                        return 2 ** 31 - 1
+                    if num > 2 ** 31 -1:
+                        return 2 ** 31 -1
+                    return num
                 else:
                     if -num < - 2 ** 31:
-                        return -2 ** 31
-
-                return num if flag == True else -num
-
-            num = num * 10 + a
+                        return - 2 ** 31
+                    return -num
         if flag:
             if num > 2 ** 31 - 1:
                 return 2 ** 31 - 1
+            return num
         else:
             if -num < - 2 ** 31:
-                return -2 ** 31
+                return - 2 ** 31
+            return -num
     else:
         return 0
 
-    return num if flag == True else -num
-
-print(atoi("  0000000000012345678"))
+print(atoi("-91283472332"))
