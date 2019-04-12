a417a6308d21a86bca044313bea09bf7b76f7f94
XuYining <oxuyining@gmail.com>
2019-04-12 16:24:23 +0800

    change numbers to roman

--- leetcode/Roman_numbers.py
+++ leetcode/Roman_numbers.py
@@ -12,5 +12,49 @@ def change_roman_num(s):
             ans += a[s[i]]
     return ans
 
-a = "MCMXCIV"
-print(change_roman_num(a))
+def change_num_roman(num):
+    """
+    :type num: int
+    :rtype s: str
+    """
+    ans = ''
+    ans += 'M' * int(num // 1000)
+    num = num % 1000
+    ans += 'CM' * int(num // 900)
+    num = num % 900
+    ans += 'D' * int(num // 500)
+    num = num % 500
+    ans += 'CD' * int(num // 400)
+    num = num % 400
+    ans += 'C' * int(num // 100)
+    num = num % 100
+    ans += 'XC' * int(num // 90)
+    num = num % 90
+    ans += 'L' * int(num // 50)
+    num = num % 50
+    ans += 'XL' * int(num // 40)
+    num = num % 40
+    ans += 'X' * int(num // 10)
+    num = num % 10
+    ans += 'IX' * int(num // 9)
+    num = num % 9
+    ans += 'V' * int(num // 5)
+    num = num % 5
+    ans += 'IV' * int(num // 4)
+    num = num % 4
+    ans += 'I' * num
+    return ans
+
+
+def chang_num_roman_faster(num):
+    ans = '' + \
+          ["", "M", "MM", "MMM"][num // 1000] + \
+          ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"][(num % 1000) // 100] + \
+          ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"][num % 100 // 10] + \
+          ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"][num % 10]
+    return ans
+
+# a = "MCMXCIV"
+# print(change_roman_num(a))
+
+print(change_num_roman(31))
--- leetcode/find_max_area.py
+++ leetcode/find_max_area.py
@@ -11,5 +11,22 @@ def maxArea(height):
                 max = max if max > height[i] * (i + 1) else height[i] * (i + 1)
     return max
 
+def maxArea_fast(height):
+    left = 0
+    right = len(height) - 1
+    maxArea = 0
+    while left < right:
+        b = right - left
+        if height[left] < height[right]:
+            h = height[left]
+            left += 1
+        else:
+            h = height[right]
+            right -= 1
+        area = b * h
+        if maxArea < area:
+            maxArea = area
+    return maxArea
 
-print(maxArea([1,8,6,2,5,4,8,3,7]))
+
+print(maxArea_fast([1,8,6,2,5,4,8,3,7]))
