c64988ed766e03ea6cd38266a0f1e4ca8dd17136
XuYining <oxuyining@gmail.com>
2019-04-12 17:36:27 +0800

    optimized longest commom prefix

--- leetcode/LongestCommonPrefix.py
+++ leetcode/LongestCommonPrefix.py
@@ -33,4 +33,31 @@ def longestCommonPrefix2(strs):
         ans = ans[0:index+1]
     return ans
 
+def longestCommonPrefix_fast(strs):
+    if not strs: return ""
+    ss = list(map(set, zip(*strs)))
+    res = ""
+    for i, x in enumerate(ss):
+        x = list(x)
+        if len(x) > 1:
+            break
+        res = res + x[0]
+    return res
+
+def longestCommonPrefix_fast2(strs):
+    if not strs: return ""
+    s1 = min(strs)
+    s2 = max(strs)
+    for i, x in enumerate(s1):
+        if x != s2[i]:
+            return s2[:i]
+    return s1
+
 print(longestCommonPrefix2(["dog","racecar","car"]))
+
+
+"""
+r = [len(set(c)) == 1 for c in zip(*strs)] + [0]
+        return strs[0][:r.index(0)] if strs else ''
+
+"""
